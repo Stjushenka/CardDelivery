6a42db90f97cf1f994d072173d49ae81a12eaf0a
Stjushenka <stjushenka@gmail.com>
2022-03-07 19:02:51 +0300

    2

--- src/test/java/RequestFormTest.java
+++ src/test/java/RequestFormTest.java
@@ -25,13 +25,33 @@ public class RequestFormTest {
         String meetingDay = dateSetUp(3);
 
         open("http://localhost:9999");
-        $(".input__control[placeholder='Город']").setValue("Киров");
+        $(".input__control[placeholder='Город']").setValue("Нижний Новгород");
         $(".input__control[placeholder='Дата встречи']").doubleClick().sendKeys(Keys.BACK_SPACE, meetingDay);
         $(".input__control[name='name']").setValue("Иванов Иван");
-        $(".input__control[name='phone']").setValue("+78889996633");
+        $(".input__control[name='phone']").setValue("+70000000000");
         $(".checkbox__box").click();
         $(".button").click();
         $(withText("Успешно!")).shouldBe(appear, Duration.ofSeconds(15));
         $(".notification__content").shouldHave(exactText("Встреча успешно забронирована на " + meetingDay));
     }
+
+    @Test
+    public void shouldSendForm1() {
+
+        DateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd");
+
+        open("http://localhost:9999");
+        $(".input__control[placeholder='Город']").setValue("Каз");
+        $(byText("Казань")).click();
+        $(".icon_name_calendar").click();
+        $$("td").find(exactText("18")).click();
+        $(".input__control[name='name']").setValue("Иванов Иван");
+        $(".input__control[name='phone']").setValue("+78889996633");
+        $(".checkbox__box").click();
+        $(".button").click();
+        $(withText("Успешно!")).shouldBe(appear, Duration.ofSeconds(15));
+        $(".notification__content").shouldHave(exactText("Встреча успешно забронирована на " + "18.03.2022"));
+    }
+
+
 }
